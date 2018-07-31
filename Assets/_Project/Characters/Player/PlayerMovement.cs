9f48c1446d4a69e722963afd573ca202b859cee6
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-07-31 18:13:54 +0200

    PlayerMovement now uses Invector namespace instead of implementing it.

--- Assets/_Project/Characters/Player/PlayerMovement.cs
+++ Assets/_Project/Characters/Player/PlayerMovement.cs
@@ -1,60 +1,56 @@
+using Invector.CharacterController;
 using System;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-namespace Invector.CharacterController
+[RequireComponent(typeof (vThirdPersonController))]
+public class PlayerMovement : MonoBehaviour
 {
-    [RequireComponent(typeof (vThirdPersonController))]
-    public class PlayerMovement : MonoBehaviour
-    {
-        [SerializeField] private float _targetThreshold = 0.2f;
-        vThirdPersonController _controller;
-        CameraRaycaster _cameraRaycaster;
-        Vector3 _currentClickTarget;
+    [SerializeField] private float _targetThreshold = 0.2f;
+    vThirdPersonController _controller;
+    CameraRaycaster _cameraRaycaster;
+    Vector3 _currentClickTarget;
         
-        private void Start()
-        {
-            _cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
-            Assert.IsNotNull(_cameraRaycaster);
+    private void Start()
+    {
+        _cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        Assert.IsNotNull(_cameraRaycaster);
 
-            _controller = GetComponent<vThirdPersonController>();
-            _currentClickTarget = transform.position;
-        }
+        _controller = GetComponent<vThirdPersonController>();
+        _currentClickTarget = transform.position;
+    }
 
-        // Fixed update is called in sync with physics
-        private void FixedUpdate()
-        {
-            HandlePointAndClickInput();
-        }
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        HandlePointAndClickInput();
+    }
 
-        private void HandlePointAndClickInput ()
+    private void HandlePointAndClickInput ()
+    {
+        // Directional Movement.
+        if (Input.GetButton("Fire2"))
         {
-            // Directional Movement.
-            if (Input.GetButton("Fire2"))
+            print("Cursor raycast hit layer: " + _cameraRaycaster.LayerHit);
+
+            switch (_cameraRaycaster.LayerHit)
             {
-                print("Cursor raycast hit layer: " + _cameraRaycaster.LayerHit);
-
-                switch (_cameraRaycaster.LayerHit)
-                {
-                    case Layer.Walkable:
-                        // Movement.
-                        _currentClickTarget = _cameraRaycaster.Hit.point;
-                        print("Hit Walkable");
-                        break;
-                    default:
-                        Debug.LogWarning("Raycasting to unhandled layer.");
-                        break;
-                }
+                case Layer.Walkable:
+                    // Movement.
+                    _currentClickTarget = _cameraRaycaster.Hit.point;
+                    print("Hit Walkable");
+                    break;
+                default:
+                    Debug.LogWarning("Raycasting to unhandled layer.");
+                    break;
             }
-            Vector3 targetDirection = _currentClickTarget - transform.position;
-            if (targetDirection.magnitude > _targetThreshold)
-            {
-                _controller.input.x = targetDirection.x;
-                _controller.input.y = targetDirection.z; 
-            }
+        }
+        Vector3 targetDirection = _currentClickTarget - transform.position;
+        if (targetDirection.magnitude > _targetThreshold)
+        {
+            _controller.input.x = targetDirection.x;
+            _controller.input.y = targetDirection.z; 
+        }
             
-        }
     }
 }
-
-
