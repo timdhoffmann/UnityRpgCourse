9732d88ecb15cd2d5d664a9c2f168a55dbf6a0ed
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-07-31 19:16:11 +0200

    New Cursor.

--- Assets/_Project/Cameras/CustomCursor.cs
+++ Assets/_Project/Cameras/CustomCursor.cs
@@ -5,6 +5,8 @@ using UnityEngine.Assertions;
 
 public class CustomCursor : MonoBehaviour 
 {
+    [SerializeField] private Texture2D _standardCursor;
+    [SerializeField] private Vector2 _standardCursorTopLeftOffset;
     private CameraRaycaster _cameraRaycaster;
 
 	// Use this for initialization
@@ -12,6 +14,8 @@ public class CustomCursor : MonoBehaviour
 	{
         _cameraRaycaster = GetComponent<CameraRaycaster>();
         Assert.IsNotNull(_cameraRaycaster);
+
+        Cursor.SetCursor(_standardCursor, _standardCursorTopLeftOffset, CursorMode.Auto);
 	}
 	
 	// Update is called once per frame
